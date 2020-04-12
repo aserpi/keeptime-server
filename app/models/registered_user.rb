fcd0d4d31468d612c3dd3b4f5296a16629d6cc1e
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-04-12 20:15:53 +0200

    Add authentication tokens to registered users

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -1,4 +1,8 @@
 class RegisteredUser < User
+  extend Devise::Models
+  devise :database_authenticatable, :recoverable, :registerable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
   validates :email, presence: true
   validates :username, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_171423) do
+ActiveRecord::Schema.define(version: 2020_04_12_175945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,12 @@ ActiveRecord::Schema.define(version: 2020_04_11_171423) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.text "tokens"
+    t.string "encrypted_password", default: "", null: false
     t.index ["email"], name: "index_registered_users_on_email", unique: true
+    t.index ["uid", "provider"], name: "index_registered_users_on_uid_and_provider", unique: true
     t.index ["username"], name: "index_registered_users_on_username", unique: true
   end
 
