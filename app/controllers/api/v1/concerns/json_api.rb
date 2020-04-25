55987bba303832e10e322285952e079c72315158
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-04-25 23:54:59 +0200

    Add JSON API concerns

--- app/controllers/api/v1/concerns/json_api.rb
+++ app/controllers/api/v1/concerns/json_api.rb
@@ -4,8 +4,81 @@ module Api
       module JsonApi
         extend ActiveSupport::Concern
 
+        included do
+          rescue_from ActionController::ParameterMissing, with: :missing_parameter
+        end
+
+        def check_client_generated_id
+          if params.require(:data).has_key?(:id)
+            render json: { errors: [{ code: :client_generated_id, source: "/data/id", status: status_s(:forbidden) }] },
+                   status: :forbidden
+            return false
+          end
+
+          true
+        end
+
+        def check_forbidden_relationships(*forbidden_rels)
+          return true unless params.require(:data).has_key?(:relationships)
+          incl_relationships = params.require(:data).permit(relationships: {})[:relationships].keys
+          forbidden_rels = forbidden_rels.map &:to_s  # Input is an array of strings
+
+          errors = []
+          incl_relationships.select { |incl_rel| forbidden_rels.include?(incl_rel) }.each do |incl_rel|
+            errors << { code: :forbidden_relationship,
+                        source: "/data/relationships/#{incl_rel}",
+                        status: status_s(:forbidden) }
+          end
+
+          return true if errors.empty?
+          render json: { errors: errors }, status: :forbidden
+          false
+        end
+
+        def check_type(type, params, prefix="")
+          if params.require(:data).permit(:type)[:type] != type.to_s
+            render json: { errors: [{ code: :wrong_type,
+                                      source: "#{prefix}/data/type",
+                                      status: status_s(:conflict) }] }, status: :conflict
+            return false
+          end
+
+          true
+        end
+
+        def check_update_id(resource)
+          if params.require(:data).require(:id) != resource.id.to_s
+            render json: { errors: [{ code: :wrong_id, source: "/data/id", status: status_s(:conflict) }] },
+                   status: :conflict
+            return false
+          end
+
+          true
+        end
+
+        def render_error(resource)
+          errors = []
+          resource.errors.details.each do |attr, attr_errors|
+            attr_errors.each { |error| errors << { code: error[:error], pointer: "/data/attributes/#{attr}" } }
+          end
+          render json: { errors: errors }, status: :bad_request
+        end
+
         def status_s(status_s)
           Rack::Utils.status_code(status_s).to_s
+        end
+
+        private
+
+        def missing_parameter(exception)
+          if exception.param == :data
+            pointer = "/data"
+          elsif exception.param == :attributes
+            pointer = "/data/attributes"
+          else
+            pointer = "/data/attributes/#{exception.param}"
+          end
+          render json: { errors: [{ code: :missing_parameter, source: pointer }] }, status: :bad_request
         end
       end
     end
